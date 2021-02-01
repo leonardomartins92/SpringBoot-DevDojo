daa0e8d60073cbcafc6d6915a75db0ed1035b579
Leonardo Rodrigues <59699766+leonardomartins92@users.noreply.github.com>
2021-02-01 15:11:20 -0300

    First

---
+++ src/main/java/academy/devdojo/springboot2/controller/HelloController.java
@@ -0,0 +1,24 @@
+package academ.devdojo.springboot2.controller;
+
+import academ.devdojo.springboot2.util.Components;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("test")
+@Log4j2
+@AllArgsConstructor
+public class HelloController {
+    
+    @GetMapping(path = "doleo")
+    public String exibe(){
+        Components components = new Components();
+        return components.exibe();
+    }
+
+}
---
+++ src/main/java/academy/devdojo/springboot2/util/Components.java
@@ -0,0 +1,10 @@
+package academ.devdojo.springboot2.util;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Components {
+
+    public String exibe(){
+        return "Olá Léo";
+    }
+}
